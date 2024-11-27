e5b8661744ba592296657e9d5679d1c4e524c5b6
Abeiku <arkohabeiku@gmail.com>
2024-11-27 08:03:59 +0000

    Assignment four

---
+++ Assignments/Assignment Four/map.js
@@ -0,0 +1,9 @@
+// Description:
+// The .map() function creates a new array by applying a specified function to each element in the original array.
+// It does not modify the original array.
+
+// Example:
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map((num) => num * num);
+
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
